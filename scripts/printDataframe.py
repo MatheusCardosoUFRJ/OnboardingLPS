131a0230d96d3e4ed195da6a0a2a7d7b624d489e
casmatheus <ID+username@users.noreply.github.com>
2026-04-18 11:25:24 -0300

    Initial Commit

---
+++ scripts/printDataframe.py
@@ -0,0 +1,26 @@
+import sys
+import pandas as pd
+
+def main():
+  if len(sys.argv) < 2:
+    print("Erro: Você deve passar o caminho do arquivo CSV como entrada.")
+    sys.exit(1)
+
+  csvPath = sys.argv[1]
+
+  try: 
+    df = pd.read_csv(csvPath)
+  except FileNotFoundError:
+    print(f"Erro: Arquivo não encontrado no caminho '{csvPath}'.")
+    sys.exit(1)
+  except pd.errors.EmptyDataError:
+    print("Erro: O arquivo CSV fornecido está vazio.")
+    sys.exit(1)
+  except Exception as e:
+    print(f"Erro inesperado ao ler o CSV: {e}")
+    sys.exit(1)
+  
+  print(df)
+
+if __name__ == "__main__":
+    main()
